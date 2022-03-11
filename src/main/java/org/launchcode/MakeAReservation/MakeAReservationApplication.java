adf2de9e3a1dc242ee24dd13eab3ce59dd90c381
Julia Schreck <jrschreck716@gmail.com>
2022-03-11 16:22:17 -0500

    initial commit

---
+++ src/main/java/org/launchcode/MakeAReservation/MakeAReservationApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.MakeAReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeAReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeAReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/MakeAReservation/MakeAReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.MakeAReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeAReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
